4f75a7b8de548c13aaa6cac5b95e3e104cf5cc36
Dmitro <trepeshite@gmail.com>
2022-11-12 21:59:15 +0200

    Homework-25

--- js/main.js
+++ js/main.js
@@ -1,46 +1,24 @@
-// !!  ДЗ 24. Слайдер базовий
+// !!!  ДЗ 25. Голосування
 
-// Пишемо свій слайдер зображень.
-// На сторінці є зображення та кнопки Next, Prev з боків від зображення.
-// При кліку на Next - показуємо наступне зображення.
-// При кліку на Prev - попереднє.
-// При досягненні останнього зображення - ховати кнопку Next. Аналогічно з першим зображенням і кнопкою Prev.
+// Створити програму для відображення результатів голосування.
+// З наступними умовами:
+// Як варіанти відповіді - смайлики. За замовчуванням, 5шт.
+// Виведення даних - смайлики в один рядок, під ними - кількість тих, хто проголосував за кожен смайл.
+// При натисканні на смайл - під ним змінюється значення лічильника.
+// Реалізувати таким чином, щоб додавання нових варіантів відповіді не вело до додавання нових функцій.
 
-// !!  Рішення
+// !!  Ріщення
 
-// пробував мінімум змінювати код html (поставив класи на buttons і поставив max-width на зображення, бо №6 - велике) і все робитии в js. бо можна ще зробити пару div і пити іншим шляхом
+/*
+1. знаходимо всі смайли
+2. слухаємо, чи натискають на один із них
+2. якщо нажали, то шукаємо сусідній елемент <p> і його значення і добавляємо йому +1
+*/
 
-document.querySelector(".previousButton").style.visibility = 'hidden'
+const images = document.querySelectorAll("img");
+images.forEach ((img) => {img.addEventListener("click", clickOnSmile)});
 
-const pictures = ["img/dog1.jpeg", "img/dog2.jpeg", "img/dog3.jpeg", "img/dog4.jpeg", "img/dog5.jpeg", "img/dog6.jpeg"];
-
-console.log(pictures.length);
-
-document.querySelector(".previousButton").addEventListener ('click', slidePrevious);
-document.querySelector(".nextButton").addEventListener ('click', slideNext);
-
-let currentSlide = 0;
-
-function slidePrevious () {
-  if (currentSlide === 1) {
-    document.querySelector(".previousButton").style.visibility = 'hidden'
-  }
-  if (currentSlide === pictures.length-1) {
-    document.querySelector(".nextButton").style.visibility = 'visible'
-  }
-  currentSlide--;
-  document.querySelector("div.container > img").src = pictures[currentSlide];
-  document.querySelector("div.container > img").alt = `dog${currentSlide+1}`;
-}
-
-function slideNext () {
-  if (currentSlide === pictures.length-2) {
-    document.querySelector(".nextButton").style.visibility = 'hidden'
-  }
-  if (currentSlide === 0) {
-    document.querySelector(".previousButton").style.visibility = 'visible'
-  }
-  currentSlide++;
-  document.querySelector("div.container > img").src = pictures[currentSlide];
-  document.querySelector("div.container > img").alt = `dog${currentSlide+1}`;
+function clickOnSmile (){
+  const counterPicture = this.nextElementSibling;
+  counterPicture.textContent = Number(counterPicture.textContent) + 1;
 }
