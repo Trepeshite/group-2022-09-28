8a2a729397ae95259402f44bf3adecfb1ab3d95b
Dmitro <trepeshite@gmail.com>
2022-11-13 22:30:03 +0200

    Homework-27

--- js/main.js
+++ js/main.js
@@ -1,46 +1,21 @@
-// !!  ДЗ 24. Слайдер базовий
+// !!  ДЗ 27. Перехід за посиланням
 
-// Пишемо свій слайдер зображень.
-// На сторінці є зображення та кнопки Next, Prev з боків від зображення.
-// При кліку на Next - показуємо наступне зображення.
-// При кліку на Prev - попереднє.
-// При досягненні останнього зображення - ховати кнопку Next. Аналогічно з першим зображенням і кнопкою Prev.
+// На сторінці інпут та кнопка.
+// При натисканні на кнопку - переходимо за посиланням, яке введено у інпут.
+// Також треба реалізувати перевірку введеного значення, чи вказаний протокол http/https.
+// Якщо протокол не вказаний - додаємо https
 
-// !!  Рішення
+// !! Рішення
 
-// пробував мінімум змінювати код html (поставив класи на buttons і поставив max-width на зображення, бо №6 - велике) і все робитии в js. бо можна ще зробити пару div і пити іншим шляхом
+const input = document.querySelector("#input");
+const button = document.querySelector("#button");
 
-document.querySelector(".previousButton").style.visibility = 'hidden'
+button.addEventListener("click", redirect);
 
-const pictures = ["img/dog1.jpeg", "img/dog2.jpeg", "img/dog3.jpeg", "img/dog4.jpeg", "img/dog5.jpeg", "img/dog6.jpeg"];
-
-console.log(pictures.length);
-
-document.querySelector(".previousButton").addEventListener ('click', slidePrevious);
-document.querySelector(".nextButton").addEventListener ('click', slideNext);
-
-let currentSlide = 0;
-
-function slidePrevious () {
-  if (currentSlide === 1) {
-    document.querySelector(".previousButton").style.visibility = 'hidden'
+function redirect () {
+  if (input.value.includes('https://') || input.value.includes('http://')) {
+    window.location = input.value;
+  } else {
+    window.location = `https://${input.value}`;
   }
-  if (currentSlide === pictures.length-1) {
-    document.querySelector(".nextButton").style.visibility = 'visible'
-  }
-  currentSlide--;
-  document.querySelector("div.container > img").src = pictures[currentSlide];
-  document.querySelector("div.container > img").alt = `dog${currentSlide+1}`;
-}
-
-function slideNext () {
-  if (currentSlide === pictures.length-2) {
-    document.querySelector(".nextButton").style.visibility = 'hidden'
-  }
-  if (currentSlide === 0) {
-    document.querySelector(".previousButton").style.visibility = 'visible'
-  }
-  currentSlide++;
-  document.querySelector("div.container > img").src = pictures[currentSlide];
-  document.querySelector("div.container > img").alt = `dog${currentSlide+1}`;
 }
